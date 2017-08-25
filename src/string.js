a0da60919302182587318c858b4faefdf094dded
cyclone77 <fillchar@live.com>
2017-08-25 22:01:45 +0800

    es6

---
+++ src/string.js
@@ -0,0 +1,22 @@
+let a = "ECMAScript";
+let b = `Hello ${a} 6`;
+console.log(b);
+
+console.log("hello |".repeat(4));
+
+//二进制申明
+let binary = 0b1001101;
+console.log(binary);
+
+//八进制申明
+let octal = 0o555;
+console.log(octal);
+
+let c = 14 / 3;
+console.log(Number.isFinite(c));
+
+let d = '9.18';
+console.log(Number.parseInt(d));
+console.log(Number.parseFloat(d));
+
+console.log(Number.MAX_SAFE_INTEGER);
